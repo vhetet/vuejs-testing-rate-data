e4bc14ef187348a79df5ada4f87e28d4eb7806bb
vhetet <vhetet@solstice.com>
2020-04-29 17:47:03 -0500

    filter out us total from the fetch data for all states since it has a dedicated script

--- scripts/fetchDataForAllStates.js
+++ scripts/fetchDataForAllStates.js
@@ -2,5 +2,5 @@ const stateList = require( '../data/state_list.json')
 const fetchData = require('./fetchData.js')
 const fetchUsTotalData = require('./fetchUsTotalData.js')
 
-stateList.filter(x => x.includes('us/')).map(x => fetchData(x.slice(3)))
+stateList.filter(x => x.includes('us/') && x !== 'us/us').map(x => fetchData(x.slice(3)))
 fetchUsTotalData()

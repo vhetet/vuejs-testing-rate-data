ab8c912c504a6dc60af027a4276e5ec41db2b128
vhetet <vhetet@solstice.com>
2020-04-29 18:54:07 -0500

    update the script

--- scripts/updateData.sh
+++ scripts/updateData.sh
@@ -1,5 +1,3 @@
-git clone git@github.com:vhetet/vuejs-testing-rate-data.git
-cd vuejs-testing-rate-data
 git checkout -b tempBranch
 npm i
 echo run the scripts
@@ -9,5 +7,3 @@ echo done running the script
 git add .
 git commit -m 'daily update'
 git push -u origin tempBranch
-cd ..
-# rm -rf vuejs-testing-rate-data

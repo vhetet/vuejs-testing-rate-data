0219f5791944449304ab7c96a5d3139d2448cf54
vhetet <vhetet@solstice.com>
2020-05-02 18:11:26 -0500

    update data update script

--- scripts/updateData.sh
+++ scripts/updateData.sh
@@ -1,15 +1,7 @@
-echo git --no-pager branch -a
-git --no-pager branch -a
-echo git --no-pager log --pretty=oneline --max-count=10
-git --no-pager log --pretty=oneline --max-count=10
-echo git branch --show-current
-git branch --show-current
 echo git fetch
 git fetch
 echo git checkout master
 git checkout master
-echo git branch --show-current
-git branch --show-current
 echo npm i
 npm i
 echo node scripts/fetchDataForAllStates.js

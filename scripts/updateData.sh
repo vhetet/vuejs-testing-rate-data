38fe09bccaa1d84d8402c745df678f777c5cd0f6
vhetet <vhetet@solstice.com>
2020-05-02 18:09:38 -0500

    update data update script

--- scripts/updateData.sh
+++ scripts/updateData.sh
@@ -10,15 +10,15 @@ echo git checkout master
 git checkout master
 echo git branch --show-current
 git branch --show-current
-# echo npm i
-# npm i
-# echo node scripts/fetchDataForAllStates.js
-# node scripts/fetchDataForAllStates.js
-# echo node scripts/fetchWorldData.js
-# node scripts/fetchWorldData.js
-# echo git add .
-# git add .
-# echo git commit -m 'daily update'
-# git commit -m 'daily update'
-# echo git push -u origin master
-# git push -u origin master
+echo npm i
+npm i
+echo node scripts/fetchDataForAllStates.js
+node scripts/fetchDataForAllStates.js
+echo node scripts/fetchWorldData.js
+node scripts/fetchWorldData.js
+echo git add .
+git add .
+echo git commit -m 'daily update'
+git commit -m 'daily update'
+echo git push -u origin master
+git push -u origin master
